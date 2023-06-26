c7bc0c0be8098d4852786394104bb2fef86d37d9
Andrey Chel <97778824+avn174@users.noreply.github.com>
2023-06-26 21:12:16 +0500

    паукпау

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        char[] charArray = inputString.toCharArray();
+
+
+        for (char c : charArray) {
+            System.out.println(c);
+        }
+
+        scanner.close();
+        System.out.println(charArray);
+    }
+}
